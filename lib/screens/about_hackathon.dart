ad8d5cd381571d7eb74acd3c0f0d354f1e626b73
NIkhilGour <nikhilgour513@gmail.com>
2024-08-14 22:28:05 +0530

    loader at webview

--- lib/screens/about_hackathon.dart
+++ lib/screens/about_hackathon.dart
@@ -1,18 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class AboutHackathon extends StatelessWidget {
+class AboutHackathon extends StatefulWidget {
   const AboutHackathon({super.key});
+
+  @override
+  _AboutHackathonState createState() => _AboutHackathonState();
+}
+
+class _AboutHackathonState extends State<AboutHackathon> {
+  bool isLoading = true;
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    const platformParams = PlatformWebViewControllerCreationParams();
+    _controller = WebViewController.fromPlatformCreationParams(platformParams)
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse('https://walmart.converge.tech/content/converge/en_in/sparkathon.html'),
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
-        body: WebViewWidget(controller: WebViewController.fromPlatformCreationParams(
-      const PlatformWebViewControllerCreationParams()
-        )..setJavaScriptMode(JavaScriptMode.unrestricted)..loadRequest(Uri.parse('https://walmart.converge.tech/content/converge/en_in/sparkathon.html'))
-
-        ),
+      appBar: AppBar(
+        title: const Text('About Hackathon'),
+        backgroundColor: const Color(0xFF0071CE),
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          if (isLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
     );
   }
 }
